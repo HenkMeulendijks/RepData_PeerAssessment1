edc6cc7297ccd229e089ff39c0dafa7ea835f58e
HenkMeulendijks <qim@dds.nl>
2016-04-26 11:35:49 +0300

    Delete knit.R

--- knit.R
+++
@@ -1,6 +0,0 @@
-require(knitr) # required for knitting from rmd to md
-require(markdown) # required for md to html 
-
-SourceFile <- "./reproducible-research/week1/RepData_PeerAssessment1/PA1_template.Rmd"
-DestinationFile  <- "./reproducible-research/week1/RepData_PeerAssessment1/PA1_template.md"
-knit(SourceFile, DestinationFile) # creates md file
